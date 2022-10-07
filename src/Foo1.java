5456b94aaa29ffc837d7c0c6562f176c741e4192
«Mikle <cobra-mikle@mail.ru>
2022-10-07 19:34:49 +0300

    Initial commit

---
+++ src/Foo1.java
@@ -0,0 +1,22 @@
+/* Описать возможность построения экземпляра класса с больши́м количеством атрибутов
+class Foo {
+    int arg1;
+    String arg2;
+    double arg3;
+    int arg4;
+    String arg5;
+    int arg6;
+    String arg7;
+}
+new Foo(arg1:1);
+        new Foo(arg1:1, arg2:"qwe");
+        new Foo(arg1:1, arg7:"qwe");
+        new Foo(arg2:"qweasd", arg7:"qwe");*/
+
+public interface Foo1 {
+
+
+    public void Foo1();
+
+}
+
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Integer [] a = new Integer[]{1,2,3,4,5};
+        Foo foo = new Foo(a,1,2,3,"dsf","dgy",1.2); // Много параметров, можно каждый раз вводить любое количество
+
+
+    }
+}
